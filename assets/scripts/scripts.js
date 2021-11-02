e5aa5ee8d58e8b5364832cea4d36d22345c725fd
wifono <daniel.trstansky@gmail.com>
2021-11-02 21:49:52 +0100

    gg

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -4,21 +4,13 @@
     let order = document.getElementsByClassName('order');
     let number = document.getElementsByClassName('number');
 
-    $(document).ready(function () {
-
-
-    
-        $(number).hide();
-
-    })
+    $(document).ready(function () { $(number).hide();})
 
     $(box).click(function(event) {
-        
         let box = event.target;
-
         let boxNum = $(box).children(".number");
  
-        $(boxNum).toggle(100);           
-        });
+    $(boxNum).toggle(100);           
+});
 
 }) (jQuery)
